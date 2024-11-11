f996201cd9b1214b72eaa0845398f522abbb91ec
Ashutosh Wagh <ashutoshwagh@Ashutoshs-Air.attlocal.net>
2024-11-11 12:00:25 -0500

    added ip 172.31.39.189

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9jozq6a)8)yvojq^%6zl*9ho2aawd18ls+^so5^01vl)t^!w0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.82.58.134','54.197.77.157']
+ALLOWED_HOSTS = ['3.82.58.134','54.197.77.157','172.31.39.189']
 
 
 # Application definition
