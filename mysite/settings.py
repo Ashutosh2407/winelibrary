3db65dbcfc687d49ce8987fdff59008ea238c6e6
Ashutosh Wagh <ashutoshwagh@Ashutoshs-Air.attlocal.net>
2024-11-13 13:43:49 -0500

    added static_root variable in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,8 @@ STATICFILES_DIRS = [
     
 ]
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
